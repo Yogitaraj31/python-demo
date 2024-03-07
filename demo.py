1138ba8f9b37fd6a12cb5e97b303fd75e8a2942d
Yogita <143957102+Yogitaraj31@users.noreply.github.com>
2024-03-07 14:04:23 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,16 @@
+a=1.1
+b="yogita"
+c=True
+d= None
+print(a)
+print(b)
+a1= 9
+print(a+a1)
+print("the type of a is " , type(a))
+print("the type of a is " , type(b))
+print("the type of a is " , type(c))
+print("the type of a is " , type(d))
+print(5+7)
+print(5-7)
+print(5**7)
+print(5//7)
